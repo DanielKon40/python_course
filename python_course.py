a29c41194b5e3d480594fe33c812d6967f486b10
DanielKon40 <danielkon40@gmail.com>
2024-09-26 16:55:35 +0300

    Create python_course.py

---
+++ python_course.py
@@ -0,0 +1,112 @@
+#hello
+#print('hello world')
+
+"""
+הערות
+"""
+
+#print('hello world')
+#print('hello world')
+
+
+x=5
+y=2
+"""
+y='boo'
+y="boo"
+x=b'3'
+y=[3]
+x={4}
+z=(3,)
+"""
+
+#print(type(z))
+
+num_a=3.333333333333333333
+#print(num_a)
+
+jeff_basoz_wealth=100000000000
+num_of_human_cells=37000000000000
+number_of_people_ever_lived=108470690115
+stars_in_the_universe=1000000000000000000000
+#print(jeff_basoz_wealth*number_of_people_ever_lived*num_of_human_cells*stars_in_the_universe)
+
+#פעולות מתמטיות
+#print(7+5)
+#print()
+
+#bitwise operators
+
+resultA=x&y
+resultB=x|y
+resultC=~x
+resultD=x^y
+resultE=x>>2
+resultF=x<<2
+
+"""
+print(bin(resultA)[2:].zfill(8))
+print(bin(resultB)[2:].zfill(8))
+print(bin(resultC)[2:].zfill(8))
+print(bin(resultD)[2:].zfill(8))
+print(bin(resultE)[2:].zfill(8))
+print(bin(resultF)[2:].zfill(8))
+"""
+num=1
+#num+=1
+#num&=1
+#num>>=1
+#num/=2
+num*=5
+#print(num)
+
+#comparison operators
+
+num_a=100
+num_b=10
+
+"""
+print(num_a==num_b)
+print(num_a!=num_b)
+print(num_a<num_b)
+print(num_a>num_b)
+print(num_a<=num_b)
+print(num_a>=num_b)
+
+"""
+
+#logical operators
+num_01=5
+num_02=6
+#print(num_01<num_02)
+
+x=True
+y=False
+z=False
+
+"""
+print(x and y)
+print(x or y)
+print(not x)
+"""
+
+#print((x and y) or z)
+
+x=2
+y=2
+z=30
+
+#print(x is y)
+#print(x is z)
+
+myStr="Hello Python"
+#print('H' in myStr)
+#print('x' not in myStr)
+
+mylist=[1,2,3,4,5]
+#print(2 not in mylist)
+
+str_a="hello"
+str_b="python"
+
+print(str_a, 'ffff', 'rrrr', str_b, sep='$', end='n')
